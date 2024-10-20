fb99ede65770a128c2965c0d04a2b3cea30fc3bc
Naif Otibi <nn.n@outlook.cl>
2024-10-21 02:10:10 +0300

    completing the component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,17 @@
+import { Moon } from "lucide-react";
 
-
-
-
-export default function Header(){
+export default function Header() {
+  return (
     <div className="py-4">
-        <div className="container mx-auto px-4">
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl">Where in the world?</h1>
-            </div>
+      <div className="container mx-auto px-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-bold">Where in the world?</h1>
+          <button className="inline-flex gap-2 items-center">
+            <Moon />
+            <span className="text-base font-semibold">Dark Mode</span>
+          </button>
         </div>
+      </div>
     </div>
+  );
 }
