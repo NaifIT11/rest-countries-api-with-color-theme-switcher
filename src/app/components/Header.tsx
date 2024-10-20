70b9c5e107e1fe73de26a0aa5784923a78ff99ee
Naif Otibi <nn.n@outlook.cl>
2024-10-21 02:05:07 +0300

    component header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Header(){
+    <div className="py-4">
+        <div className="container mx-auto px-4">
+            <div className="flex items-center justify-between">
+                <h1 className="text-xl">Where in the world?</h1>
+            </div>
+        </div>
+    </div>
+}
