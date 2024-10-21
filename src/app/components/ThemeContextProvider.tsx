5db91c3a1e8d709aa6224575b6749e0fd84b5a54
Naif Otibi <nn.n@outlook.cl>
2024-10-22 00:26:15 +0300

    edit something

--- src/app/components/ThemeContextProvider.tsx
+++ src/app/components/ThemeContextProvider.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { ThemeProvider } from './ThemeContext';
-import Header from './Header';
 
 const ThemeProviderWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <ThemeProvider>
-            <Header />
             {children}
         </ThemeProvider>
     );
